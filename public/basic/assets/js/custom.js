5752f26fca578ae3d6e2b6b8a5c73e07df8b690d
Tyler Warren <warrejt@yahoo.com>
2016-08-24 08:18:55 -0500

    adding about me page

--- public/basic/assets/js/custom.js
+++ public/basic/assets/js/custom.js
@@ -7,6 +7,5 @@
 		});  	
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
 	}); 
-
 		
 })(jQuery);
